104ea31b370b91f380d2532f9e26cc08178222ff
Yuzgin <122491012+Yuzgin@users.noreply.github.com>
2024-08-17 13:43:15 +0100

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,8 +1,5 @@
-# Uncomment the following imports before adding the Model code
-
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
-
 
 # Create your models here.
 
@@ -26,13 +23,15 @@ class CarModel(models.Model):
         # Add more choices as required
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2023, 
-                               validators=
-                               [
-                                MaxValueValidator(2023), 
-                                MinValueValidator(2015)
-                               ])
-    
+    year = models.IntegerField(
+        default=2023,
+        validators=[
+            MaxValueValidator(2023), 
+            MinValueValidator(2015)
+        ]
+    )
+
     # Other fields as needed
     def __str__(self):
         return self.name  # Return the name as the string representation
+
